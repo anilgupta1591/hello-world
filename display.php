f230b832174975edaa83612746b531bbf002ae72
yogeshshukla <shuklayogesh065@gmail.com>
2018-05-10 01:42:51 +0530

    codes

---
+++ display.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Display email</title>
+</head>
+<body>
+	<!DOCTYPE html>
+  <html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+  
+ <table>
+   <tr>
+     <th>Name</th>
+     <th>Email</th>
+   </tr>
+ 
+  <?php
+  error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$servername = "mysql-instance1.claqtdx0ejhl.us-east-1.rds.amazonaws.com";
+$username = "anil1591";
+$password = "itechuser";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, name,email FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "<tr><td>" ;
+       echo  $row["name"];
+       echo  '</td><td>'.$row["email"];
+       echo  '</td></tr>';
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+</table>
+</body>
+</html>
---
+++ edit.php
@@ -0,0 +1,40 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$servername = "mysql-instance1.claqtdx0ejhl.us-east-1.rds.amazonaws.com";
+$username = "anil1591";
+$password = "itechuser";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+ ?>      
+       <form method="post"> 
+       	<input type="hidden" name="id" value="<?php echo $id; ?>">
+       <input type="text" name="subject_mail" style="
+    width:  300px;
+"><br />
+       <input type="text" name="body" /><br />
+       <input type="submit" name="submit">
+   </form>
+
+
+<?php
+if(isset($_POST['submit'])){
+	$subject_mail = $_POST['subject_mail'];
+	$body = $_POST['body'];
+	$sql = "insert into user (name,email) values('".$subject_mail."','".$body."')";
+  if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+  } else {
+      echo "Error updating record: " . $conn->error;
+  }
+
+}
+
+$conn->close();
+?>
