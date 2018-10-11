8000b90be5445144e39ba73f86a3e4ec60b7ad15
Anil <hs.anil.gupta@gmail.com>
2018-10-11 15:23:11 +0530

    tesd

--- display.php
+++ display.php
@@ -16,7 +16,7 @@
      <th>Name</th>
      <th>Email</th>
    </tr>
- 
+ asdasdasdas
   <?php
   error_reporting(E_ALL);
 ini_set('display_errors', 1);
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 Hello this this testing file
+<?php
+echo "Hello world";
+
+?>
